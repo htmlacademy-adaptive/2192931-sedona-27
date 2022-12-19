0b7c2ef3c658996f851d601f833bfd4a54f36a7d
Aleksey <111198139+MrTruth34@users.noreply.github.com>
2022-12-19 19:09:42 +0300

    Исправляет недочеты

--- source/js/script.js
+++ source/js/script.js
@@ -1,10 +1,7 @@
+const menuBtn = document.querySelector(".main-nav__toggle");
+const menu = document.querySelector(".main-nav__list");
 
-const menuBtn = document.querySelector('.main-nav__toggle');
-const menu = document.querySelector('.main-nav__list');
-
-menuBtn.addEventListener('click', ()=>{
-  menu.classList.toggle('main-nav__list--active');
-  menuBtn.classList.toggle('main-nav__toggle--active');
-})
-
-
+menuBtn.addEventListener("click", () => {
+  menu.classList.toggle("main-nav__list--active");
+  menuBtn.classList.toggle("main-nav__toggle--active");
+});
