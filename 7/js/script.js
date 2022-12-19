9d6dfd308c30efb23684358c2d78a4196d4a3a34
Keks <keks@htmlacademy.ru>
2022-12-19 11:11:42 +0300

    :heavy_check_mark: Сборка #7

--- 7/js/script.js
+++ 7/js/script.js
@@ -1,3 +1,10 @@
 
+const menuBtn = document.querySelector('.main-nav__toggle');
+const menu = document.querySelector('.main-nav__list');
+
+menuBtn.addEventListener('click', ()=>{
+  menu.classList.toggle('main-nav__list--active');
+  menuBtn.classList.toggle('main-nav__toggle--active');
+})
 
 
